edeb3810896d8c8a68c2fcc2c670ecdd8a2031d5
adame <adammfh91@gmail.com>
2025-12-24 12:21:15 +0100

    adam

---
+++ Professeur/style.js
@@ -0,0 +1,32 @@
+
+const form = document.getElementById("profForm");
+const tableBody = document.getElementById("tableBody");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const data = [
+        nom.value,
+        prenom.value,
+        matiere.value,
+        niveau.value,
+        frais.value,
+        tel.value
+    ];
+
+    const row = document.createElement("tr");
+
+    data.forEach(text => {
+        const td = document.createElement("td");
+        td.textContent = text;
+        row.appendChild(td);
+    });
+
+    tableBody.appendChild(row);
+
+    // petite animation JS
+    row.style.opacity = "0";
+    setTimeout(() => row.style.opacity = "1", 50);
+
+    form.reset();
+});
